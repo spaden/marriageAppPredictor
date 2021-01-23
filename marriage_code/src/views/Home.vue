9edefa43cc311b0d98ed625188c95b9f6006408f
Ruchitha-Deshoju <rrruchi56@gmail.com>
2021-01-23 23:55:01 +0530

    button

--- marriage_code/src/views/Home.vue
+++ marriage_code/src/views/Home.vue
@@ -4,6 +4,9 @@
 			“We meet our soulmates when we’re on our soul path.”
 			<span>– Karen M. Black</span>
 		</div>
+		<div class="goButton">
+			<button type="button" class="btn">Check it out</button>
+		</div>
 	</div>
 </template>
 
@@ -22,9 +25,9 @@ export default class Home extends Vue {}
 .home {
 	background-image: url("../../src/assets/splashScreen4_veryHigh.jpg");
 	background-size: cover;
-	background-position: center;
-	margin-top: 10px;
-	margin-bottom: 10px;
+	background-position: center top;
+	margin-bottom: 20px;
+	margin-top: 15px;
 	width: 98%;
 	height: 97vh;
 	border-radius: 30px;
@@ -34,13 +37,13 @@ export default class Home extends Vue {}
 	align-items: center;
 	font-family: "Caveat", cursive;
 	font-size: 36px;
+	color: #258970;
 	.heading_box {
-		color: #258970;
 		padding: 20px;
 		border-radius: 15px;
 		width: auto;
 		height: auto;
-		transform: translate(0, -200px);
+		transform: translate(15px, -200px);
 		background: rgba(255, 255, 255, 0.9);
 		box-shadow: 1px 5px 5px 0px rgba(0, 0, 0, 0.25);
 		position: relative;
@@ -53,6 +56,22 @@ export default class Home extends Vue {}
 			bottom: 0;
 			color: #054d48;
 			right: 10px;
+		}
+	}
+	.goButton {
+		color: #000000;
+		font-family: Arial, Helvetica, sans-serif;
+		transform: translate(-370px, -50px);
+		font-family: "Changa", sans-serif;
+
+		font-weight: 500;
+		.btn {
+			border-radius: 7px;
+			padding: 10px;
+			padding-left: 20px;
+			padding-right: 20px;
+			font-size: 18px;
+			background: rgba(33, 123, 101, 0.51);
 		}
 	}
 }
