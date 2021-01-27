741d23b0c35b4f4c43f95195c37f90b8afc171d9
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2021-01-28 00:47:27 +0530

    image_option_view

--- marriage_code/src/views/Quiz_web.vue
+++ marriage_code/src/views/Quiz_web.vue
@@ -1,8 +1,8 @@
 <template>
 	<div id="container">
-		<div class="box">
+		<div v-if="multipleChoiceView" class="box">
 			Choose your {{ colorName }}
-		
+
 			<div class="box__choose">
 				<span class="circle circle__left">
 					<svg height="100" width="100">
@@ -21,38 +21,132 @@
 				</span>
 			</div>
 		</div>
+		<div v-else class="box box__imageview">
+
+			<div class="image">
+				<img width="400px" height="385px"
+					src="https://github.com/spaden/hookdate.github.io/blob/master/five.jpg?raw=true">
+			</div>
+			<div class="buttonsSelect">
+				Do you like the picture?
+				<div class="emojiButtons">
+				    <div class="emojiText">
+						<img width="50px"
+							height="50px"
+							src="/assets/in_love.PNG"/>
+						<p>Yes</p>
+					</div>
+					<div class="emojiText">
+						<img width="50px"
+							height="50px"
+							src="/assets/vomit.PNG"/>
+						<p>No</p>
+					</div>
+					<div class="emojiText">
+						<img width="50px"
+							height="50px"
+							src="/assets/observer.PNG"/>
+						<p>Not Really</p>
+					</div>
+
+				</div>
+				
+
+			</div>
+
+		</div>
 	</div>
 </template>
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator"
-@Component
-export default class QuizPageWeb extends Vue {
-	colorName = "blue"
-}
+	import {
+		Component,
+		Vue
+	} from "vue-property-decorator"
+	@Component
+	export default class QuizPageWeb extends Vue {
+		multipleChoiceView = false
+		colorName = "blue"
+
+	}
 </script>
 <style lang="scss">
-#container {
-	display: flex; /* establish flex container */
-	flex-direction: column; /* make main axis vertical */
-	justify-content: center; /* center items vertically, in this case */
-	align-items: center;
-	height: 100vh;
-	.box {
-		font-size: 30px;
-		font-family: "Changa", sans-serif;
-		font-weight: bold;
-		background-color: rgba(255, 255, 255, 0.2);
-		backdrop-filter: blur(5px);
-		border-radius: 10px;
-		padding: 50px;
-		transform: translateY(-100px);
+	#container {
+		display: flex;
+		/* establish flex container */
+		flex-direction: column;
+		/* make main axis vertical */
+		justify-content: center;
+		/* center items vertically, in this case */
+		align-items: center;
+		height: 100vh;
 
-		&__choose {
-			margin-top: 30px;
-			.circle {
-				margin: 60px;
+		.box {
+			font-size: 30px;
+			font-family: "Changa", sans-serif;
+			font-weight: bold;
+			background-color: rgba(255, 255, 255, 0.2);
+			backdrop-filter: blur(5px);
+			border-radius: 15px;
+			padding: 50px;
+			transform: translateY(-100px);
+
+			&__choose {
+				margin-top: 30px;
+
+				.circle {
+					margin: 60px;
+					filter: drop-shadow(3px 4px 4px rgba(0, 0, 0, 0.25));
+
+				}
+			}
+
+			&__imageview {
+				margin: 10px;
+				padding: 5px !important;
+				position: relative;
+				display: flex;
+				flex-direction: row;
+				flex-wrap: wrap;
+				justify-content: space-evenly;
+				.image {
+					img {
+						border-radius: 15px;
+					    filter: drop-shadow(8px 5px 7px rgba(0, 0, 0, 0.25));
+					}
+				}
+				
+				.buttonsSelect {
+					margin: 40px;
+					margin-left: 60px;
+					margin-right: 60px;
+					font-size: 24px;
+					display: flex;
+					flex-direction: column;
+					flex-wrap: wrap;
+					.emojiButtons {
+					  margin-top: 10px;
+					  width: 100%;
+					  display: flex;
+					  flex-direction: column;
+					  align-items: flex-start;
+					  flex-wrap: wrap;
+					  .emojiText {
+						  margin-top: 30px;
+						  display: flex;
+						  flex-wrap: wrap;
+						  flex-direction: row;
+						  img {
+							  margin-right: 50px;
+							  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+						  }
+						  p {
+							  margin-top: 10px;
+							  font-size: 20px;
+						  }
+					  }
+					}
+				}
 			}
 		}
 	}
-}
 </style>
